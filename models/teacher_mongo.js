746d131071f93a6d624f92337fbd3be3bf79503d
Ferdie Nodalo <ferdienodalo7@gmail.com>
2023-06-13 19:21:35 +0800

    Ferdie

--- models/teacher_mongo.js
+++ models/teacher_mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const teacherSchema = new mongoose.Schema({
-<<<<<<< HEAD
 	email: {
 		type: String,
 		required: true,
@@ -30,44 +29,10 @@ const teacherSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
-	type: {
-		type: String,
-		required: true,
+	createdAt: {
+		type: Date,
+		default: () => Date.now(),
 	},
-=======
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  lname: {
-    type: String,
-    required: true,
-  },
-  fname: {
-    type: String,
-    required: true,
-  },
-  contact: {
-    type: String,
-    required: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: () => Date.now(),
-  },
->>>>>>> my-branch
 });
 
 module.exports = mongoose.model("Teachers", teacherSchema);

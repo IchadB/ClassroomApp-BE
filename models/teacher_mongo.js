e4d6f69a92cab11bb41650390baca38806e2f5a2
IchadB <richardbetalmos@gmail.com>
2023-06-13 19:25:42 +0800

    updating express

--- models/teacher_mongo.js
+++ models/teacher_mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const teacherSchema = new mongoose.Schema({
-<<<<<<< HEAD
   email: {
     type: String,
     required: true,
@@ -26,56 +25,14 @@ const teacherSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  img: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
   age: {
     type: String,
     required: true,
   },
-  joinedDate: {
+  createdAt: {
     type: Date,
     default: () => Date.now(),
   },
-=======
-	email: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	lname: {
-		type: String,
-		required: true,
-	},
-	fname: {
-		type: String,
-		required: true,
-	},
-	contact: {
-		type: String,
-		required: true,
-	},
-	gender: {
-		type: String,
-		required: true,
-	},
-	age: {
-		type: String,
-		required: true,
-	},
-	createdAt: {
-		type: Date,
-		default: () => Date.now(),
-	},
->>>>>>> 746d131071f93a6d624f92337fbd3be3bf79503d
 });
 
 module.exports = mongoose.model("Teachers", teacherSchema);

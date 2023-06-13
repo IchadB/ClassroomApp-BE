8eab2ebec9a538f38b34a829ee172c98826db66a
IchadB <richardbetalmos@gmail.com>
2023-06-13 11:14:57 +0800

    merge

--- models/teacher_mongo.js
+++ models/teacher_mongo.js
@@ -1,40 +1,6 @@
 const mongoose = require("mongoose");
 
 const teacherSchema = new mongoose.Schema({
-<<<<<<< HEAD
-	email: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	lname: {
-		type: String,
-		required: true,
-	},
-	fname: {
-		type: String,
-		required: true,
-	},
-	contact: {
-		type: String,
-		required: true,
-	},
-	gender: {
-		type: String,
-		required: true,
-	},
-	age: {
-		type: String,
-		required: true,
-	},
-	type: {
-		type: String,
-		required: true,
-	},
-=======
   email: {
     type: String,
     required: true,
@@ -67,7 +33,6 @@ const teacherSchema = new mongoose.Schema({
     type: Date,
     default: () => Date.now(),
   },
->>>>>>> my-branch
 });
 
 module.exports = mongoose.model("Teachers", teacherSchema);

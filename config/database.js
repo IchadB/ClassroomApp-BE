da91382ee7aada730247a855c1bde3982897f545
Ferdie Nodalo <ferdienodalo7@gmail.com>
2023-06-20 22:27:21 +0800

    Ferdie nothing to commit working tree clean

--- config/database.js
+++ config/database.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
-// const MONGO_URL =
-//   "mongodb+srv://IchadB:ichadB@cluster0.2epkefm.mongodb.net/ClassroomAppDB?retryWrites=true&w=majority";
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    // mongoose.connection.once("open", () => {
-    console.log(`Mongo DB Connected: ${conn.connection.host}`);
-    //   });
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		// mongoose.connection.once("open", () => {
+		console.log(`Mongo DB Connected: ${conn.connection.host}`);
+		//   });
+	} catch (error) {
+		console.log(error);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDB;

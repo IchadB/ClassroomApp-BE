da850471cd785d0753d23e070822da97d8089b2b
IchadB <richardbetalmos@gmail.com>
2023-06-21 20:35:50 +0800

    added a filter query

--- routes/students.routes.js
+++ routes/students.routes.js
@@ -6,25 +6,26 @@ const studentsController = require("../controllers/students.controller");
 
 routes.get("/get-students", protectRoutes, studentsController.getAllStudents);
 routes.post(
-	"/attendance-students",
-	protectRoutes,
-	studentsController.addStudentAttendance
+  "/attendance-students",
+  protectRoutes,
+  studentsController.addStudentAttendance
 );
 routes.get(
-	"/get-attendances",
-	protectRoutes,
-	studentsController.getAllAttendanceStudents
+  "/get-attendances",
+  protectRoutes,
+  studentsController.getAllAttendanceStudents
 );
 routes.get(
-	"/student/:email",
-	protectRoutes,
-	studentsController.getStudentByEmail
+  "/student/:email",
+  protectRoutes,
+  studentsController.getStudentByEmail
 );
 routes.get("/exam/:id", protectRoutes, studentsController.getStudentExamById);
+routes.get("/exams", protectRoutes, studentsController.getExams);
 routes.post(
-	"/exam-answers",
-	protectRoutes,
-	studentsController.answeredStudentExams
+  "/exam-answers",
+  protectRoutes,
+  studentsController.answeredStudentExams
 );
 
 module.exports = routes;

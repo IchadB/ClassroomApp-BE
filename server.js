9d4de945cc28be9afce91747a0da16fa75aa14d9
IchadB <richardbetalmos@gmail.com>
2023-06-22 21:48:50 +0800

    solution 3 for cors problem

--- server.js
+++ server.js
@@ -20,30 +20,27 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "https://classroom-app-fe.vercel.app",
-    credentials: true,
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Credentials",
-    ],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://classroom-app-fe.vercel.app",
+//     credentials: true,
+//     allowedHeaders: [
+//       "Content-Type",
+//       "Authorization",
+//       "Access-Control-Allow-Credentials",
+//     ],
+//   })
+// );
 
-app.options("/users/login", (req, res) => {
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Access-Control-Allow-Credentials"
-  );
-  res.header(
+app.use((req, res, next) => {
+  res.setHeader(
     "Access-Control-Allow-Origin",
     "https://classroom-app-fe.vercel.app"
   );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.status(200).send();
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
 });
 
 app.use("/teachers", teacherRouter);

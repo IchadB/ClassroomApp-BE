d4efecfc89297f3cfcd6c9ba18c2368461575fb9
IchadB <richardbetalmos@gmail.com>
2023-06-22 19:58:39 +0800

    deploying backend

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://classroom-app-fe.vercel.app",
     credentials: true,
     allowedHeaders: [
       "Content-Type",

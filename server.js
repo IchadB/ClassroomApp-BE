287391d0de18cebfc203609b4704ba19c00d866b
IchadB <richardbetalmos@gmail.com>
2023-06-22 21:55:14 +0800

    solution 4

--- server.js
+++ server.js
@@ -38,7 +38,10 @@ app.use((req, res, next) => {
     "https://classroom-app-fe.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Access-Control-Allow-Credentials"
+  );
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });

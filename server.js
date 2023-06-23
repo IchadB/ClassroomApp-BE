1dabe7b3763f98c350bc8563a5145072c7f5375e
IchadB <richardbetalmos@gmail.com>
2023-06-23 10:00:17 +0800

    updates

--- server.js
+++ server.js
@@ -32,20 +32,6 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://classroom-app-fe.vercel.app"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, Access-Control-Allow-Credentials"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   next();
-// });
-
 app.use("/teachers", teacherRouter);
 app.use("/students", studentsRouter);
 app.use("/users", usersRouter);

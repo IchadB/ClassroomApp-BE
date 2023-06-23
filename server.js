2b933330f3a954f84e57631c79f8d80e7408b4a9
IchadB <richardbetalmos@gmail.com>
2023-06-23 13:03:45 +0800

    configure

--- server.js
+++ server.js
@@ -14,23 +14,19 @@ const usersRouter = require("./routes/users.routes");
 dotenv.config();
 connectDB();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Access-Control-Allow-Credentials",
-    ],
-  })
-);
 
 app.use("/teachers", teacherRouter);
 app.use("/students", studentsRouter);
